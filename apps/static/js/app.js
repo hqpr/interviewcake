95c853338c73f467372a753584eb2ca5d2d3faa9
hqpr <adubnyak@gmail.com>
2018-11-20 14:56:41 +0200

    CSRF_COOKIE_SECURE to False

--- apps/static/js/app.js
+++ apps/static/js/app.js
@@ -4,6 +4,7 @@ var myApp = angular.module('myApp', []).config(function($interpolateProvider, $h
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 });
 
 myApp.config(['$qProvider', function ($qProvider) {

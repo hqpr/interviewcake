16bb20f49c6f531952e3d1ba730733790378e10b
hqpr <adubnyak@gmail.com>
2018-11-20 14:15:52 +0200

    csrf_exempt on views

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import ReadingViewSet, UpdateReadingCourseView, UpdateQuestionCourseView
 
 urlpatterns = [
-    url(regex=r'^courses/readings/$', view=ReadingViewSet.as_view({'get': 'list'}), name='courses'),
-    url(regex=r'^courses/r/(?P<pk>[-\w]+)/$', view=UpdateReadingCourseView.as_view(), name='update_reading'),
-    url(regex=r'^courses/q/(?P<pk>[-\w]+)/$', view=UpdateQuestionCourseView.as_view(), name='update_question'),
+    url(r'^courses/readings/$', ReadingViewSet.as_view({'get': 'list'}), name='courses'),
+    url(r'^courses/r/(?P<pk>[-\w]+)/$', csrf_exempt(UpdateReadingCourseView.as_view()), name='update_reading'),
+    url(r'^courses/q/(?P<pk>[-\w]+)/$', csrf_exempt(UpdateQuestionCourseView.as_view()), name='update_question'),
 ]
